7250224185afbad36ea04bfe4897216825961e3b
Majba UL Hoque <majba9399@gmail.com>
2024-10-19 12:30:59 +0600

    fixed navbar responsive issue & errorpage

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
             <div className="relative">
                 <img src={Bg} alt="" className='w-[100%] h-[90vh] object-cover' />
                 <div className="absolute inset-0 flex flex-col justify-center items-center">
-                    <h1 className="text-5xl font-bold mb-4">Welcome to STEADfast IT</h1>
+                    <h1 className="text-5xl font-bold mb-4 text-center">Welcome to STEADfast IT</h1>
                     <p className="text-2xl mb-6">Your Trusted It Partner</p>
                     <button className="px-4 py-2 bg-black text-white hover:bg-white hover:text-black" onClick={openModal}>Learn More</button>
                 </div>
